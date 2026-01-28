ad835ff01a0e460596eadbb05909efd58c3fb4c6
Kurt Biñas <kcbinas@student.apc.edu.ph>
2026-01-28 17:11:43 +0800

    Finished Task 3

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,19 +1,12 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
- 
-const instruments = ref([])
- 
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
- onMounted(() => {
-   getInstruments()
-})
-</script>
- <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
+<template>
+  <h1>Profile Webpage</h1>
+  <pfp/>
+  <h1>Food</h1>
+  <food-item/>
+  <h1>Fun Fact</h1>
+  <anime/>  
 </template>
+
+<script></script>
+
+<style></style>
