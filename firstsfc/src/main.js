27dad41d30e2523bb58d59c73cfa1115539b9437
Kurt Biñas <kcbinas@student.apc.edu.ph>
2026-01-28 17:15:56 +0800

    .

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import Anime from './components/Anime.vue'
 import Pfp from './components/PersonalProfile.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('anime', Anime)
